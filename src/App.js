a70b7a97add91388dc4eefbc48153c0b54cbfb9f
riddhish.k@somaiya.edu <riddhish.k@somaiya.edu>
2024-03-03 16:12:40 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="navbar">
+      <a href="#home">HOME</a>
+      <a href="#projects">PROJECTS</a>
+      <a href="#hobbies">HOBBIES</a>
+    </div>
+  );
+}
+
+export default App;
