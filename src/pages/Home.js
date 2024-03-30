72ead383223def9e021ad3508521fb6d674ac307
riddhish.k@somaiya.edu <riddhish.k@somaiya.edu>
2024-03-30 18:04:33 +0530

    BuildingHomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,123 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./Home.css";
+import {
+  SiLeetcode,
+  SiLinkedin,
+  SiGithub,
+  SiInstagram,
+} from "@icons-pack/react-simple-icons";
 
 function Home() {
+  const [style1, setStyle1] = useState("mediumhr");
+  const [style2, setStyle2] = useState("smallhr");
+  const [style3, setStyle3] = useState("smallhr");
+  const [blur1, SetBlur1] = useState("removeblur");
+  const [blur2, SetBlur2] = useState("makeblur");
+  const [blur3, SetBlur3] = useState("makeblur");
+
   return (
     <div className="home">
       <div className="west">
-        <h1 style={{ fontSize: "4rem", color: "#000000" }}>Riddhish Khot</h1>
+        <div>
+          <h1 className="name">Riddhish Khot</h1>
+          <h3 className="name">Software Developer</h3>
+          <h4 className="name">
+            Bringing digital dreams to life with code craftsmanship.
+          </h4>
+        </div>
+        <div className="navigator">
+          <div
+            className={`navigator-child ${blur1}`}
+            onMouseEnter={() => {
+              setStyle1("mediumhr");
+              SetBlur1("removeblur");
+            }}
+            onMouseLeave={() => {
+              setStyle1("smallhr");
+              SetBlur1("makeblur");
+            }}
+          >
+            <hr className={style1} />
+            <h4 className="name">About</h4>
+          </div>
+          <div
+            className={`navigator-child ${blur2}`}
+            onMouseEnter={() => {
+              setStyle2("mediumhr");
+              SetBlur2("removeblur");
+            }}
+            onMouseLeave={() => {
+              setStyle2("smallhr");
+              SetBlur2("makeblur");
+            }}
+          >
+            <hr className={style2} />
+            <h4 className="name">Experience</h4>
+          </div>
+          <div
+            className={`navigator-child ${blur3}`}
+            onMouseEnter={() => {
+              setStyle3("mediumhr");
+              SetBlur3("removeblur");
+            }}
+            onMouseLeave={() => {
+              setStyle3("smallhr");
+              SetBlur3("makeblur");
+            }}
+          >
+            <hr className={style3} />
+            <h4 className="name">Projects</h4>
+          </div>
+        </div>
+        <div className="socialMedia">
+          <div className="icons">
+            <a
+              href="https://www.linkedin.com/in/riddhish-khot/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icons"
+            >
+              <SiLinkedin size={24} color="#000000" />
+            </a>
+          </div>
+          <div className="icons">
+            <a
+              href="https://github.com/ridd12"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icons"
+            >
+              <SiGithub size={24} color="#000000" />
+            </a>
+          </div>
+          <div className="icons">
+            <a
+              href="https://leetcode.com/ridk/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icons"
+            >
+              <SiLeetcode size={24} color="#000000" />
+            </a>
+          </div>
+          <div className="icons">
+            <a
+              href="https://www.instagram.com/riddhish_khot/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SiInstagram size={24} color="#000000" />
+            </a>
+          </div>
+        </div>
       </div>
       <div className="east">
-        {/* <h1 style={{ fontSize: "4rem", color: "white" }}>List Of Projects</h1> */}
-        <div className="company">
-          <h1
-            style={{ color: "white", fontSize: "2rem" }}
-            className="companyName"
-          >
-            RKS Cloud
-          </h1>
-          <h1
-            style={{ color: "white", fontSize: "2rem" }}
-            className="companyName"
-          >
-            Mumbai, MH
-          </h1>
+        <div>
+          <h3>Story</h3>
         </div>
-        <h1 style={{ color: "white" }}>
-          Full Stack Software Development Engineer{" "}
-        </h1>
-        <ol>
-          <li style={{ maxWidth: "500px", color: "#7a7c8a" }}>
-            Built In-App-purchase Auto-renewable subscriptions for iOS and
-            Android mobile applications. Used server-to-server notification to
-            handle subscription lifecycle flow. I used AWS API-Gateway, Lambda,
-            and SQS to handle notifications asynchronously.
-          </li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
       </div>
     </div>
   );
 }
-
-// RKS Cloud                                                                                           Mumbai, MH
-// Full Stack Software Development Engineer                                       Aug 2023- Present
-// • Built In-App-purchase Auto-renewable subscriptions for iOS and Android mobile
-// applications. Used server-to-server notification to handle subscription lifecycle flow.
-// I used AWS API-Gateway, Lambda, and SQS to handle notifications asynchronously.
-// • Integrated Stripe one-time payment and subscriptions in web application, managing
-// payment lifecycles through server webhooks.
-// • Contributed to building distributed pub-sub event-driven Cron job service for usage
-// insights for the onboarded customer's data using reactive spring and AWS SQS.
-// • Built onboarding system for AWS and GCP accounts using IAM STSRoles and Service
-// accounts. With the successful implementation of a Reactive Spring application, leveraging
-// AWS Cognito and AWS KMS for user authentication and robust data encryption
-// respectively. Streamlined secrets retrieval through GraphQL APIs
 
 export default Home;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,12 @@
 import nextLogo from "./next.png";
-import "./App.css";
+import "./Main.css";
 import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <div className="home">
+    <div className="main">
       <div className="mainColoum1">
-        <h1 style={{ fontSize: "5rem" }} className="name">
+        <h1 style={{ fontSize: "5rem" }} className="fisrtname">
           Riddhish K
         </h1>
         {/* <h1 style={{ fontSize: "5rem" }}>Khot</h1> */}
@@ -24,16 +24,3 @@ function Main() {
 }
 
 export default Main;
-
-// const fetchUser = async () => {
-//   fetch("http://localhost:8080/get/users")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       data.map((user) => {
-//         console.log("User " + JSON.stringify(user));
-//       });
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
